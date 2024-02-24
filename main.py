fe42916499d4477a633edb5a8975342c2ab59339
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-02-24 09:35:40 +0100

    create reward to go, evaluate longest episode

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 from tinygrad import Tensor, nn
 from tinygrad.dtype import dtypes
+import numpy as np
 
 EPISODES = 50
 BATCH_SIZE = 5000
@@ -30,27 +31,38 @@ env = gym.make("CartPole-v1")
 model = RLModel()
 opt = nn.optim.Adam(nn.state.get_parameters(model), LR)
 
+def cummulative_rewards(ep_rews):
+    return [sum(ep_rews)] * len(ep_rews)
+
+def reward_to_go(rews):
+    n = len(rews)
+    rtgs = np.zeros_like(rews)
+    for i in reversed(range(n)):
+        rtgs[i] = rews[i] + (rtgs[i+1] if i+1 < n else 0)
+    return rtgs
+
+def rewards_to_go(ep_rews):
+    ep_rews.reverse()
+    return np.flip(np.cumsum(ep_rews)).tolist()
+
 if __name__ == "__main__":
     for i in range(EPISODES):
-        actions, states, rewards, losses = [],[],[],[]
+        actions, states, rewards, losses, lengths = [],[],[],[],[]
         while True:
-            ep_len = 0
-            reward = 0
+            ep_rews = []
             done = False
             obs, _ = env.reset()
             while not done: 
-                ep_len += 1
                 states.append(obs)
                 Tensor.no_grad=True
                 action = model(Tensor(obs)).multinomial().realize().item()
                 Tensor.no_grad=False
                 obs, rew, done, _, _= env.step(action)
                 actions.append(action)
-                reward += float(rew)
-            rewards += [reward] * ep_len
+                ep_rews.append(rew)
+                lengths.append(len(ep_rews))
+            rewards += rewards_to_go(ep_rews)
             if len(actions) > BATCH_SIZE:
                 break
         losses.append(trainStep(model, Tensor(states), Tensor(rewards), Tensor(actions, dtype=dtypes.int8)))
-        print(f"episode {i} rewards = {sum(rewards)/len(rewards)}, avg_loss = {(sum(losses)/len(losses))}")
-
-
+        print(f"episode {i} rewards = {sum(rewards)/len(rewards)}, avg_loss = {(sum(losses)/len(losses))}, longest episode = {max(lengths)}")
