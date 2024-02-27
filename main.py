a3c320ef581407dd7edace916e7d7e6caf02c3d0
Szymon Ożóg <58388001+SzymonOzog@users.noreply.github.com>
2024-02-27 16:10:41 +0100

    Actor critic

--- main.py
+++ main.py
@@ -17,19 +17,30 @@ class RLModel:
         x = self.fc2(x)
         return x.softmax()
 
-def trainStep(model, state, reward, actions):
-    with Tensor.train():
-        act = model(state)
-        taken = act[Tensor.arange(act.shape[0]),actions]
-        loss = -(taken.log()*(reward)).mean()
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-    return loss.numpy()
+def vanilla_loss(model, buffer):
+    act = model(Tensor(buffer["states"]))
+    taken = act[Tensor.arange(act.shape[0]),Tensor(buffer["actions"], dtype=dtypes.uint8)]
+    loss = -(taken.log() * Tensor.stack(buffer["rewards"])).mean()
+    return loss
 
-env = gym.make("CartPole-v1")
-model = RLModel()
-opt = nn.optim.Adam(nn.state.get_parameters(model), LR)
+def actor_critic_loss(model, buffer):
+    act = model(Tensor(buffer["states"]))
+    taken = act[Tensor.arange(act.shape[0]),Tensor(buffer["actions"], dtype=dtypes.uint8)]
+    advantage = calc_advantage(buffer)
+    loss = -(taken.log() * advantage).mean()
+    critic_loss = advantage.pow(2).mean()
+    # print(loss.numpy(), critic_loss.numpy(), advantage.numpy())
+    return loss + critic_loss
+
+class Critic:
+    def __init__(self):
+        self.fc1 = nn.Linear(in_features=4, out_features=32)
+        self.fc2 = nn.Linear(in_features=32, out_features=1)
+
+    def __call__(self, x):
+        x = self.fc1(x).tanh()
+        x = self.fc2(x)
+        return x.flatten()
 
 def cummulative_rewards(ep_rews):
     return [sum(ep_rews)] * len(ep_rews)
@@ -45,24 +56,57 @@ def rewards_to_go(ep_rews):
     ep_rews.reverse()
     return np.flip(np.cumsum(ep_rews)).tolist()
 
+def calc_advantage(buffer, gamma=0.99):
+    q_vals = Tensor.zeros(0)
+    values = Tensor.zeros(0)
+    for vals, rews in zip(buffer["values"], buffer["rewards"]):
+        discounts = np.power(gamma, np.arange(len(rews)))
+        q = Tensor(rews,dtype=dtypes.float) * Tensor(discounts,dtype=dtypes.float)
+        q_vals = Tensor.cat(q_vals, q)
+        values = Tensor.cat(values, Tensor.stack(vals).flatten())
+    return q_vals - values
+
+env = gym.make("CartPole-v1")
+model = RLModel()
+critic = Critic()
+opt = nn.optim.Adam(nn.state.get_parameters(model), LR)
+
+buffer = {}
+
 if __name__ == "__main__":
     for i in range(EPISODES):
-        actions, states, rewards, losses, lengths = [],[],[],[],[]
+        buffer["actions"] = []
+        buffer["states"] = []
+        buffer["losses"] = []
+        buffer["rewards"] = []
+        buffer["values"] = []
+        buffer["lengths"] = []
         while True:
             ep_rews = []
+            ep_vals = []
             done = False
             obs, _ = env.reset()
             while not done: 
-                states.append(obs)
+                buffer["states"].append(obs)
                 Tensor.no_grad=True
                 action = model(Tensor(obs)).multinomial().realize().item()
                 Tensor.no_grad=False
+                with Tensor.train():
+                    value = critic(Tensor(obs))
+                ep_vals.append(value)
                 obs, rew, done, _, _= env.step(action)
-                actions.append(action)
+                buffer["actions"].append(action)
                 ep_rews.append(rew)
-                lengths.append(len(ep_rews))
-            rewards += rewards_to_go(ep_rews)
-            if len(actions) > BATCH_SIZE:
+                buffer["lengths"].append(len(ep_rews))
+            buffer["rewards"].append(rewards_to_go(ep_rews))
+            buffer["values"].append(ep_vals)
+            if len(buffer["actions"]) > BATCH_SIZE:
                 break
-        losses.append(trainStep(model, Tensor(states), Tensor(rewards), Tensor(actions, dtype=dtypes.int8)))
-        print(f"episode {i} rewards = {sum(rewards)/len(rewards)}, avg_loss = {(sum(losses)/len(losses))}, longest episode = {max(lengths)}")
+        with Tensor.train():
+            opt.zero_grad()
+            loss = actor_critic_loss(model, buffer)
+            loss.backward()
+            opt.step()
+        buffer["losses"].append(loss.numpy())
+        print(f"episode {i}, avg_loss = {(sum(buffer['losses'])/len(buffer['losses']))}, longest episode = {max(buffer['lengths'])}")
+
